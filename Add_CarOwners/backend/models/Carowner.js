7585e4733d58e0acc4e1d873c78451651380fc10
MalinEkanayake31 <malinekanayake22@gmail.com>
2024-06-19 14:27:30 +0530

    first commit

---
+++ Add_CarOwners/backend/models/Carowner.js
@@ -0,0 +1,37 @@
+const mangoose = require('mangoose');
+
+const Schema = mangoose.Schema;
+
+const CarownerSchema = new Schema({
+
+    name : {
+        type : String,
+        required : true
+    },
+
+    nic : {
+        type : String,
+        require : true
+    },
+
+    age : {
+        type : Number,
+        required : true
+    },
+
+    gender : {
+        type : String,
+        required : true
+    },
+
+    address : {
+        type : String,
+        required : true
+    }
+
+})
+
+//imandi
+const Carowner = mongoose.model("Carowner",CarownerSchema);
+
+module.exports = Carowner;
---
+++ Add_CarOwners/backend/routes/carowners.js
@@ -0,0 +1,89 @@
+const router = require("express").Router();
+const Carowner = require("../models/Carowner.js");
+
+//CREATE
+router.route("/add").post((req,res) =>{
+
+    const name = req.body.name;
+    const nic = req.body.nic;
+    const age = Number(req.body.age);
+    const gender = req.body.gender;
+    const address = req.body.address;
+
+    const newCarowner = new Carowner({
+        name,
+        nic,
+        age,
+        gender,
+        address
+    })
+
+    newCarowner.save().then(()=>{
+        res.json("Car Owner Added")
+    }).catch((err)=>{
+        console.log(err);
+    })
+
+})
+
+//READ
+router.route("/").get((req,res) =>{
+
+    Carowner.find().then((carowners) =>{
+        res.json(carowners)
+    }).catch((err) =>{
+        console.log(err)
+    })
+
+})
+
+//UPDATE
+router.route("/update/:id").put(async (req,res) =>{
+
+    let userId = req.params.id;
+    const {name,nic,age,gender,address} = req.body;
+
+    const updateCarowner = {
+        name,
+        nic,
+        age,
+        gender,
+        address
+    }
+
+    const update = await Carowner.findByIdAndUpdate(userId,updateCarowner).then(() =>{
+        res.status(200).send({status : "user updated"})
+    }).catch((err) =>{
+        console.log(err);
+        res.status(500).send({status : "Error with updating data"});
+    })
+
+})
+
+//DELETE
+router.route("/delete/:id").delete(async (req,res) =>{
+
+    let userId = req.params.id;
+    
+    await Carowner.findByIdAndDelete(userId).then(() =>{
+        res.status(200).send({status : "user deleted"})
+    }).catch((err) =>{
+        console.log(err.message);
+        res.status(500).send({status : "Error with delete user" , error : err.message});
+    })
+
+})
+
+router.route("/get/:id").get(async (req,res) =>{
+
+    let userId = req.params.id;
+    await Carowner.findById(userId).then((carowner) =>{
+        res.status(200).send({status : "user fetched" , carowner})
+    }).catch((err) =>{
+        console.log(err.message);
+        res.status(500).send({status : "Error with get user" , error : err.message});
+    })
+
+})
+
+module.exports = router;
