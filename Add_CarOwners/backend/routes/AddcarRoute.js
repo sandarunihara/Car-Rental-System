0646d98bb9f2a3421300494c2fc7a77cffb5025a
ASHAN2021 <akh8130@gmail.com>
2024-07-05 22:38:52 +0530

    complete getcar,updatecar,deletecar,fetchcar api

--- Add_CarOwners/backend/routes/AddcarRoute.js
+++ Add_CarOwners/backend/routes/AddcarRoute.js
@@ -1,8 +1,12 @@
 import express from 'express';
-import { Addcars } from '../Controllers/AddcarController.js';
+import { Addcars ,getcars,updatecar,deletecar,fetchcar} from '../Controllers/AddcarController.js';
 
 const router = express.Router();
 
-router.post("/create",Addcars);
+router.post("/createcar",Addcars);
+router.get("/getcar",getcars);
+router.put("/updatecar/:id",updatecar);
+router.delete("/deletecar/:id",deletecar)
+router.get("/fetchcar/:id",fetchcar);
 
 export default router;
