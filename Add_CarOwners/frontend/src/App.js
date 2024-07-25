b983dfb9f82c42f57c00e092f6d591c96e7887d2
sandarunihara <sandarunihara56@gmail.com>
2024-07-25 10:47:08 +0530

    land

--- Add_CarOwners/frontend/src/App.js
+++ Add_CarOwners/frontend/src/App.js
@@ -1,27 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './Components/NavBar';
+import Home from './Pages/Home';
+import Footer from './Components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-
-
-          Imandi dfd sandaru
-          asa
-
-        </a>
-      </header>
+    <div>
+      <NavBar/>
+      <Home/>
+      <Footer/>
     </div>
   );
 }
---
+++ Add_CarOwners/frontend/src/Components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='h-[75px] '>Footer</div>
+  )
+}
+
+export default Footer
