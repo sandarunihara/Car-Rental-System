ad0f699ad93559d7fb053e0786c6b28138211385
Imandi Muthugala <imandividaramuthugala@gmail.com>
2024-07-31 09:51:30 +0530

    feedback

---
+++ Add_CarOwners/frontend/src/Pages/Feedback.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import NavBar from '../Components/NavBar';
+
+const Feedback = () => {
+  return (
+    <>
+    <NavBar/>
+    <div className='bg-black absolute top-0 left-0 w-full h-full object-cover -z-10'>
+
+    </div>
+    </>
+  )
+}
+
+export default Feedback
