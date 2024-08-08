70c201b7dc7be81d94160ada5cdd5b4836c9c6a3
sandarunihara <sandarunihara56@gmail.com>
2024-08-08 11:56:28 +0530

    search

--- Add_CarOwners/frontend/src/App.js
+++ Add_CarOwners/frontend/src/App.js
@@ -15,6 +15,7 @@ import AddVehicle from "./Pages/AddVehicle";
 import { DisplayFeedback } from "./Pages/DisplayFeedback";
 import Message from "./Pages/Message";
 import { UpdateVehicle } from "./Pages/UpdateVehicle";
+import Searchpage from "./Pages/Searchpage";
 
 function App() {
   return (
@@ -24,6 +25,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/carpage" element={<CarRentPage />} />
         <Route path="/Signup" element={<SignUp />} />
+        <Route path="/search" element={<Searchpage />} />
         <Route path="/Login" element={<Login />} />
         <Route path="/feedback" element={<Feedback />} />
         <Route path="/admin/*" element={<AdminLayout />} />
