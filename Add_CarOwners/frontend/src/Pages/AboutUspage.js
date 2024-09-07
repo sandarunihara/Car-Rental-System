d2ca5952c8eaa5bba8892c8c9e2da14779a2bddf
ASHAN2021 <akh8130@gmail.com>
2024-09-07 22:03:46 +0530

    complete about us

--- Add_CarOwners/frontend/src/Pages/AboutUspage.js
+++ Add_CarOwners/frontend/src/Pages/AboutUspage.js
@@ -4,11 +4,16 @@ import Member2 from '../img/sadaru.jpeg'
 import Member3 from '../img/imandi.jpg'
 import Member4 from '../img/malin.jpeg'
 import Member5 from '../img/sushmitha.jpeg'
+import { MdCloseFullscreen } from 'react-icons/md'
+import { useNavigate } from 'react-router-dom'
 
 const AboutUspage = () => {
+
+    const navigate=useNavigate();
   return (
     <div className='bg-gradient-to-r from-gray-200 to-blue-200 w-full h-screen overflow-auto'>
         <h1 className='text-5xl font-bold text-center mt-10'>About Us</h1>
+        <MdCloseFullscreen onClick={()=>{navigate('/')}} className='-mt-10 mr-10 absolute text-4xl hover:scale-125 transition-all duration-500 cursor-pointer right-0'/>
         <div className=' mt-10'>
             <div className='w-full p-10'>
                 <p className='text-2xl'>We are a car rental company that offers a wide range of vehicles for hire. We have been in business for over 20 years and have a reputation for providing top-quality service to our customers. Our fleet includes cars, trucks, and SUVs, so you can find the perfect vehicle for your needs. Whether you need a car for a day, a week, or longer, we have you covered. Our friendly staff is here to help you find the right vehicle and answer any questions you may have. We look forward to serving you!</p>
